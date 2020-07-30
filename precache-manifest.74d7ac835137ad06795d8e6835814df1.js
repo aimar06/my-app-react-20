2c0fd1161c02f63925ffe5e07bd1960a259ab2bd
Aimar Prieto <aimarprieto.54@gmail.com>
2020-07-29 20:44:11 -0500

    Updates

---
+++ precache-manifest.74d7ac835137ad06795d8e6835814df1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ca9ee934b5213a15ba710fcb92134f",
+    "url": "/my-app-react-20/index.html"
+  },
+  {
+    "revision": "2e4b75ea879da7e5c38a",
+    "url": "/my-app-react-20/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app-react-20/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react-20/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4b75ea879da7e5c38a",
+    "url": "/my-app-react-20/static/js/main.a4182e52.chunk.js"
+  },
+  {
+    "revision": "25ac4c8d3aff486ee5e6",
+    "url": "/my-app-react-20/static/js/runtime-main.a3d03b10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react-20/static/media/logo.5d5d9eef.svg"
+  }
+]);
